0bce3af703a1a4903717a8c2cc8d5377da944036
Vitor Silva <vitorgilcs@gmail.com>
2022-07-16 12:13:39 +0100

    Correct Singleton eager initialization - adding a private constructor

--- src/CreationalPatterns/Singleton/example1/Singleton_Eager_Initialization.java
+++ src/CreationalPatterns/Singleton/example1/Singleton_Eager_Initialization.java
@@ -4,6 +4,8 @@ public class Singleton_Eager_Initialization {
 
     private static final Singleton_Eager_Initialization firstInstance = new Singleton_Eager_Initialization();
 
+    private Singleton_Eager_Initialization(){}
+
     public static Singleton_Eager_Initialization getInstance(){
         return firstInstance;
     }
